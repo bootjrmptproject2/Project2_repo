14489c2ba436f834f68c0b7ed13f474ac3e2066b
Paul <psong2010@gmail.com>
2019-02-24 11:48:51 -0800

    editing logic

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,46 @@
-function createMap(schools) {
+  // Changes XML to JSON
+function xmlToJson(xml) {
+	
+	// Create the return object
+	var obj = {};
 
-// Create the tile layer that will be the background of our map
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
+
+function createMap(schools) {
+    
+    // Create the tile layer that will be the background of our map
+
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
       maxZoom: 18,
@@ -8,44 +48,46 @@ function createMap(schools) {
       accessToken: map_api
     });
   
-// Create a baseMaps object to hold the lightmap layer
+    // Create a baseMaps object to hold the lightmap layer
     var baseMaps = {
       "Light Map": lightmap
     };
   
-// Create an overlayMaps object to hold the schoollayer
+    // Create an overlayMaps object to hold the schoollayer
     var overlayMaps = {
       "Schools": schools
     };
   
-// Create the map object with options
+    // Create the map object with options
     var map = L.map("map", {
       center: [37.8272, -122.2913],
-      zoom: 12,
+      zoom: 50,
       layers: [lightmap, schools]
     });
   
-// Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
     }).addTo(map);
-  }
-  
+}
+
   function createMarkers(response) {
-  
-// Pull the "schools" property off of response.data
-    var schools = response.schools;
+    
+    console.log(xmlToJson(response))
+    var convertedSchools = xmlToJson(response)
+// Pull the "school" property off of response.schools
+    var schools = convertedSchools.schools;
 
 // Initialize an array to hold school markers
     var schoolMarkers = [];
   
 // Loop through the school array
     for (var index = 0; index < schools.length; index++) {
-      var school = schools[index];
+      var school = schools.school[index];
   
-// For each station, create a marker and bind a popup with the station's name
+// For each school, create a marker and bind a popup with the school's name
       var schoolMarker = L.marker([school.lat, school.lon])
-        .bindPopup("<h3>" + school.name + "<h3><h3>Type: " + school.type + "<h3>" + "<h3><h3>Parent Rating: " + school.parentRating);
+        .bindPopup("<h3>" + school.name + "<h3><h3>Type: " + school.gsRating + "<h3>" + "<h3><h3>Parent Rating: " + school.parentRating);
   
  // Add the marker to the schoolMarkers array
       schoolMarkers.push(schoolMarker);
@@ -56,18 +98,52 @@ function createMap(schools) {
   }
   
   function optionChanged(value) {
+    console.log(value)
     switch(value) {
         case "clusteredHeatMap":
-        d3.xml("https://api.greatschools.org/schools/nearby?key=98d1830833800c9133ce4854f15ba749&state=CA&lat=37.8272&lon=-122.2913&radius=20&limit=100", createMarkers);
-          break;
+        d3.xml(url, createMarkers);
+
+        break;
         // case y:
           // code block
           // break;
         default:
           // code block
-       }
-  }
+       }  
+  };
   
   // Perform an API call to the Great Schools API to get station information. Call createMarkers when complete
-  d3.xml("https://api.greatschools.org/schools/nearby?key=98d1830833800c9133ce4854f15ba749&state=CA&lat=37.8272&lon=-122.2913&radius=20&limit=100", createMarkers);
+  for (var index = 0; index < 5; index++) {
+    var zip = zipcodes[index]
+    var url = "https://cors-anywhere.herokuapp.com/https://api.greatschools.org/schools/nearby?key=" + school_api + "&state=CA&schoolType=public&zip="+ zip + "limit=4"
+    d3.xml(url, function(err, data){
+      console.log(data)
+    });
+  };
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
