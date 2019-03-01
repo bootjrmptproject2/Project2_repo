315369e8721890b00c4c21f4e504f22ba36d9f13
Appamomo <maurop13@gmail.com>
2019-02-28 21:00:26 -0800

    solving map visualization

--- WebPage_APP/static/js/logic2.js
+++ WebPage_APP/static/js/logic2.js
@@ -1,19 +1,4 @@
-
-
-var map = L.map("map", {
-    center: [37.8272, -122.2913],
-    zoom: 6
-  });
-
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-    maxZoom: 10,
-    id: "mapbox.light",
-    accessToken: map_api
-  });
-
-
-  
+ 
   var changedSelection = document.getElementById('selGraph');
 
   changedSelection.addEventListener("change", function() {
@@ -21,12 +6,28 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/
     switch(changedSelection.value) {
         case "clusteredHeatMap":
         console.log("chose clusteredHeatMap");
+
+
+        var map = L.map("map", {
+            center: [37.8272, -122.2913],
+            zoom: 6
+            // maxZoom: 50
+          });
+
+        L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+            maxZoom: 10,
+            id: "mapbox.streets",
+            accessToken: map_api
+          }).addTo(map);
+
         d3.json("data/Geoschooldata.json").then(function(response) {
             // console.log(response);
             var schools = response.features;
             var markers = L.markerClusterGroup();
             console.log("went into the json function");
             for (var i = 0; i < schools.length; i++) {
+                console.log("went into the loop");
                 var location = schools[i].geometry;
                 var school = schools[i].properties;
                 // console.log([school.lat, school.lon])
